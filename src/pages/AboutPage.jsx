1c47f341a64a00b23a076f4a1bbe398dd1549475
Aryan Powar <aryapowar.10@gmail.com>
2025-06-27 11:29:20 +0530

    Aryan third commit

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
             <Header/>
             <div className="content">
                 <div className="page-container" style={{overflow: "auto"}}>
-                    <h3 className="animate-fade-in-up">About Vivekanand College</h3>
+                    <h1 className="animate-fade-in-up">About Vivekanand College</h1>
                     <p>Vivekanand College stands as a beacon of knowledge and integrity, committed to delivering transformative education since its inception in [Year of Establishment, e.g., 1980]. Located in the bustling heart of [City/Area, e.g., Chembur, Mumbai], our college has consistently strived to uphold the highest standards of academic rigor and ethical values</p>
                     <p>Our vision is to empower students with critical thinking skills, a global perspective, and a strong sense of social responsibility. We aim to nurture individuals who are not only successful in their careers but also contributing members of society.</p>
                      <h2>Our Mission</h2>
